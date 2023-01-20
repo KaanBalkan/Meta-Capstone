ba5a13e3979e326dfba5e1e119f90cc769ce4cde
Kaan <mhmt.kaan@gmail.com>
2023-01-19 19:38:12 -0800

    01/19/23 Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import Footer from './Footer';
+import Nav from './Nav';
+import Hero from './Hero';
+import About from './About';
+import Highlights from './Highlights';
+import Testimonials from './Testimonials';
 
 function App() {
   return (
-    <div className="App">
-        homepage
-        Homepafe ssss
+    <>
+    <div class="container">
+      <Nav/>
+      <Hero/>
+      <Highlights />
+      <Testimonials />
+      <About/>
+      <Footer />
     </div>
+    </>
   );
 }
 
